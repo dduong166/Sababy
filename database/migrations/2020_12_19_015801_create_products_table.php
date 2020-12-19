c31fa3a2c42897a7b2e24103eec37eeef961fa48
dantokoro <duongdang0508@gmail.com>
2020-12-19 10:27:13 +0700

    model&migration

---
+++ database/migrations/2020_12_19_015801_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('product_id');
+            $table->integer('owner_id');
+            $table->string('product_name');
+            $table->text('description')->nullable();
+            $table->float('price');
+            $table->float('sale_price')->default(0);
+            $table->float('amount');
+            $table->float('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
